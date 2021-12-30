971f817cff63385c97fb62e109640e0ed2c1ab9c
atawarno <atawarno@gmail.com>
2021-12-30 06:32:29 +0200

    remove_from_data

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,29 +1,47 @@
-import './app.css';
+import { Component } from "react";
+
 import SearchPanel from "../search-panel/search-panel";
 import AppFilter from "../app-filter/app-filter";
 import AppInfo from "../app-info/app-info";
 import EmployeesList from "../employees-list/employees-list";
 import EmployeesAddForm from "../employees-add-form/employees-add-form";
 
-function App(){
+import './app.css';
 
-    const dataFromServer = [
-        { name: 'John D.', salary: 800, increase: false, id: 1},
-        { name: 'Max S.', salary: 3000, increase: true, id: 2},
-        { name: 'Steven H.', salary: 1800, increase: false, id: 3},
-    ];
+class App extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            dataFromServer: [
+                { name: 'John D.', salary: 800, increase: false, id: 1},
+                { name: 'Max S.', salary: 3000, increase: true, id: 2},
+                { name: 'Steven H.', salary: 1800, increase: false, id: 3},
+            ],
+        }
+    }
 
-    return (
-        <div className="app">
-            <AppInfo/>
-            <div className="search-panel">
-                <SearchPanel/>
-                <AppFilter/>
+    deleteItem = (id) => {
+        this.setState(({dataFromServer}) => {
+            const index = dataFromServer.findIndex(elem => elem.id === id);
+            console.log(index);
+        })
+    }
+
+    render() {
+        return (
+            <div className="app">
+                <AppInfo/>
+                <div className="search-panel">
+                    <SearchPanel/>
+                    <AppFilter/>
+                </div>
+                <EmployeesList
+                    data={this.state.dataFromServer}
+                    onDelete={this.deleteItem}/>
+                <EmployeesAddForm/>
             </div>
-            <EmployeesList data={dataFromServer}/>
-            <EmployeesAddForm/>
-        </div>
-    );
+        );
+    }
 }
 
 export default App;
