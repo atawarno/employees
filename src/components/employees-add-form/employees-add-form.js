ec5eb4e228a28454d25440a47f16646def87f0b1
atawarno <atawarno@gmail.com>
2021-12-31 02:50:10 +0200

    todo_done

--- src/components/employees-add-form/employees-add-form.js
+++ src/components/employees-add-form/employees-add-form.js
@@ -1,7 +1,8 @@
-import { Component } from "react";
+import { Component } from 'react';
+
 import './employees-add-form.css';
 
-class EmployeesAddForm extends Component{
+class EmployeesAddForm extends Component {
     constructor(props) {
         super(props);
         this.state = {
@@ -16,6 +17,16 @@ class EmployeesAddForm extends Component{
         })
     }
 
+    onSubmit = (e) => {
+        e.preventDefault();
+        if (this.state.name.length < 3 || !this.state.salary) return;
+        this.props.onAdd(this.state.name, this.state.salary);
+        this.setState({
+            name: '',
+            salary: ''
+        })
+    }
+
     render() {
         const {name, salary} = this.state;
 
@@ -23,7 +34,8 @@ class EmployeesAddForm extends Component{
             <div className="app-add-form">
                 <h3>Добавьте нового сотрудника</h3>
                 <form
-                    className="add-form d-flex">
+                    className="add-form d-flex"
+                    onSubmit = {this.onSubmit}>
                     <input type="text"
                            className="form-control new-post-label"
                            placeholder="Как его зовут?"
